ad7e14c9f7b058f497ba48b0a71b06f9769dbd92
NaokiShimatani <ps14adb2c@gmail.com>
2018-11-09 18:10:12 +0900

    change from Dropbox to Github

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 # Dropbox 
-ln -fs ~/Dropbox/dotfiles/.bash_profile ~/.bash_profile
-ln -fs ~/Dropbox/dotfiles/.bashrc ~/.bashrc
-ln -fs ~/Dropbox/dotfiles/.vim ~/.vim
-ln -fs ~/Dropbox/dotfiles/.zprofile ~/.zprofile 
-ln -fs ~/Dropbox/dotfiles/.zshrc ~/.zshrc
-ln -fs ~/Dropbox/dotfiles/.zshenv ~/.zshenv
+#ln -fs ~/Dropbox/dotfiles/.bash_profile ~/.bash_profile
+#ln -fs ~/Dropbox/dotfiles/.bashrc ~/.bashrc
+#ln -fs ~/Dropbox/dotfiles/.vim ~/.vim
+#ln -fs ~/Dropbox/dotfiles/.zprofile ~/.zprofile 
+#ln -fs ~/Dropbox/dotfiles/.zshrc ~/.zshrc
+#ln -fs ~/Dropbox/dotfiles/.zshenv ~/.zshenv
 # Github
-#ln -fs ~/dotfiles/.bash_profile ~/.bash_profile
-#ln -fs ~/dotfiles/.bashrc ~/.bashrc
-#ln -fs ~/dotfiles/.vim ~/.vim
-#ln -fs ~/dotfiles/.zprofile ~/.zprofile 
-#ln -fs ~/dotfiles/.zshrc ~/.zshrc
-#ln -fs ~/dotfiles/.zshenv ~/.zshenv
+ln -fs ~/dotfiles/.bash_profile ~/.bash_profile
+ln -fs ~/dotfiles/.bashrc ~/.bashrc
+ln -fs ~/dotfiles/.vim ~/.vim
+ln -fs ~/dotfiles/.zprofile ~/.zprofile 
+ln -fs ~/dotfiles/.zshrc ~/.zshrc
+ln -fs ~/dotfiles/.zshenv ~/.zshenv
 
 if [ ! -e ~/Dropbox/dotfiles/.vim/bundle/Vundle.vim/ ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
